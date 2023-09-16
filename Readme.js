8c041e2453a06ca31f3b9e27daa1910a1b0c42e7
Vital-pro <vitupikov@yandex.ru>
2023-09-16 17:18:51 +0300

    added function

--- Readme.js
+++ Readme.js
@@ -22,8 +22,37 @@
 //*      Ответ CLI: Untracked files:
 //* (use "git add <file>..." to include in what will be committed)
 //! newfile.js
-//*  Красный, значит, появился новый файл, гит его не отслеживает. Добавим его для отслеживания гитом, если мы будем делать изменения в этом файле
+//*  Красный, значит, появился новый файл, гит его не отслеживает. Добавим его для отслеживания гитом. Если мы будем делать изменения в этом файле, гит нам покажет
 //? git add <newfile> (для именно этого файла) или 
 //? git add .  (Точка в конце! для вообще всех новых )
 //* И теперь, если проверим состояние git, то уже увидим изменения:
 //? git status
+//*     Ответ CLI:   On branch master
+//*  No commits yet
+//* Changes to be committed: (use "git rm --cached <file>..." to unstage)
+//* new file: newfile.js   (уже будет зеленый)
+//!  Теперь для окончания работы с файлом нужно сделать commit. 
+//* Закоммитить - значит зафиксировать изменения в памяти гита
+//? git commit -m 'Start project'
+//* закоммитить с message о том или ином изменении
+//*     Ответ CLI: (кратко, т.к. файл был просто пустой)
+//* [master (root-commit) 048f8c7] Start project
+//* 1 files changed, 29 insertions(+)
+//* create mode 100644 newfile.js
+//! Посмотреть список коммитов можно командой
+//? git log
+//*     Ответ CLI: commit f8c74024b4293cf53a2bb8d5ee40bdb4bc8e8 (HEAD -> master)
+//* Author: Vital-pro <Vital-pro@yandex.ru>
+//* Date:   Sat Sep 16 14:53:55 2023 +0300
+//*  Start project
+
+//! Следующий этап. Сделаем изменения в файле (-ах)
+function( a, b) {
+  return a * b;
+}
+//* И теперь, если проверим состояние git, то уже увидим изменения:
+//? git status
+//!  modified:   newfile.js
+//todo Пасмотрим, как гит видит наши изменения
+//? git diff newfile.js
+
